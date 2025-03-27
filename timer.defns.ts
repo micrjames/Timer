01f6c22fb5d5335ca7fbf786e89c80b118329f06
Michael James <micrjames921@gmail.com>
2025-03-27 03:01:22 -0700

    created 'Timer definitions'

---
+++ timer.defns.ts
@@ -0,0 +1,30 @@
+enum TimerState {
+	STOPPED = 'STOPPED',
+	RUNNING = 'RUNNING',
+	PAUSED = 'PAUSED',
+}
+
+interface TimerEvents {
+	onStart?: () => void;
+	onStop?: () => void;
+	onError?: (error: Error) => void;
+}
+
+interface TimerConfig {
+	intervalMs: number;
+	autoStart?: boolean;
+}
+
+interface Logger {
+	log: (message: string) => void;
+	error: (message: string, error?: Error) => void;
+}
+
+class TimerError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = 'TimerError';
+	}
+}
+
+export { TimerState, TimerEvents, TimerConfig, Logger, TimerError };
