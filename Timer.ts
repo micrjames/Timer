d867c736e982f547fe58cd68aaa06eae89fd8228
Michael James <michaelrjamesjr@icloud.com>
2025-05-20 11:38:14 -0700

    wrote operations (start/stop/pause/resume) methods on 'Timer' class; completed 'Operations' test block

--- Timer.ts
+++ Timer.ts
@@ -7,7 +7,60 @@ class Timer {
 	private intervalMS: number;
 
 	constructor(intervalMS: number) {
+		this.timerId = null;
+		this.state = TimerState.STOPPED;
+		this.elapsedMS = 0;
+
+		if(intervalMS <= 0)
+			throw new Error("Interval must be a positive number");
 		this.intervalMS = intervalMS;
+	}
+
+	public start() {
+		if (this.state !== TimerState.STOPPED) {
+			throw new Error(`Cannot start: timer is ${this.state}`);
+		}
+		this.state = TimerState.RUNNING;
+		this.timerId = setInterval(() => {
+			this.elapsedMS += this.intervalMS;
+		}, this.intervalMS);
+	}
+
+	public stop() {
+		if (this.state === TimerState.STOPPED) {
+            throw new Error('Cannot stop: timer already stopped');
+        }
+        clearInterval(this.timerId!);
+        this.timerId = null;
+        this.state = TimerState.STOPPED;
+        this.elapsedMS = 0;
+	}
+
+	public pause() {
+		if (this.state !== TimerState.RUNNING) {
+            throw new Error(`Cannot pause: timer is ${this.state}`);
+        }
+        clearInterval(this.timerId!);
+        this.timerId = null;
+        this.state = TimerState.PAUSED;
+	}
+
+	public resume() {
+		if (this.state !== TimerState.PAUSED) {
+            throw new Error(`Cannot resume: timer is ${this.state}`);
+        }
+        this.state = TimerState.RUNNING;
+        this.timerId = setInterval(() => {
+            this.elapsedMS += this.intervalMS;
+        }, this.intervalMS);
+	}
+
+	public getElapsedMS(): number {
+		return this.elapsedMS;
+	}
+
+	public getState(): TimerState {
+		return this.state;
 	}
 }
 
--- tests/timer.test.ts
+++ tests/timer.test.ts
@@ -3,16 +3,49 @@ import { TimerState } from "../timer.defns";
 
 describe("Timer", () => {
 	let timer: Timer;
+	let state: TimerState;
+	let expectedState: TimerState;
 
 	beforeEach(() => {
 		timer = new Timer(1000);	// 1 second interval
 	});
+	afterEach(() => {
+		state = timer.getState();
+		if(state !== TimerState.STOPPED && state !== TimerState.PAUSED)
+			timer.stop();
+	});
 
-	test.todo("Should start the timer.");
-	test.todo("Should stop the timer.");
-	test.todo("Should pause and resume the timer.");
-	test.todo("Should throw error when starting an already running timer.");
-	test.todo("Should throw error when stopping an already stopped timer.");
-	test.todo("Should throw error when pausing a stopped timer.");
-	test.todo("Should throw error when resuming a running timer.");
+	describe("Operations", () => {
+		test("Should start the timer.", () => {
+			timer.start();
+			state = timer.getState();
+			expectedState = TimerState.RUNNING;
+			expect(state).toBe(expectedState);
+		});
+		test("Should stop the timer.", () => {
+			timer.start();
+			timer.stop();
+			state = timer.getState();
+			expectedState = TimerState.STOPPED;
+			expect(state).toBe(expectedState);
+		});
+		test("Should pause and resume the timer.", () => {
+			timer.start();
+			timer.pause();
+			state = timer.getState();
+			expectedState = TimerState.PAUSED;
+			expect(state).toBe(expectedState);
+
+			timer.resume();
+			state = timer.getState();
+			expectedState = TimerState.RUNNING;
+			expect(state).toBe(expectedState);
+		});
+	});
+	describe("Mis-Operations", () => {
+		test.todo("Should throw error when starting an already running timer.");
+		test.todo("Should throw error when stopping an already stopped timer.");
+		test.todo("Should throw error when pausing a stopped timer.");
+		test.todo("Should throw error when resuming a running timer.");
+	});
 });
